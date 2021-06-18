9ed083106dbae9a481b66eb38fefb362c840346b
Gabriel Mori Baleta <moribaleta.work@gmail.com>
2021-06-19 07:57:15 +0800

    socket io use

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
-const path = require('path')
-const express = require('express')
-
+const path      = require('path')
+const express   = require('express')
+const http      = require('http')
+const socketIO  = require('socket.io')
 
 const publicPath = path.join(__dirname + "/../public")
 const port = process.env.PORT || 3000
 
-var app = express()
+let app    = express()
+let server = http.createServer(app)
+let io     = socketIO(server)
 
 app.use(express.static(publicPath))
 
-
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
+
+io.on('connection', (socket) => {
+    console.log('a new user just connected')
+
+    socket.on('disconnect', () =>{
+        console.log('user was disconnected')
+    })
+})
+
